3269aea5a7950a163d8674d72a2d7b1fe479c7d0
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-22 23:41:29 +0530

    vector basic

--- vector/vector_1.cpp
+++ vector/vector_1.cpp
@@ -15,5 +15,15 @@ int main()
     v.push_back(1);
     cout << "Size:" << v.size() << endl;
     cout << "Capacity:" << v.capacity() << endl;
+    v.push_back(2);
+    cout << "Size:" << v.size() << endl;
+    cout << "Capacity:" << v.capacity() << endl;
+    v.push_back(4);
+    cout << "Size:" << v.size() << endl;
+    cout << "Capacity:" << v.capacity() << endl;
+    v.resize(5);
+    cout << "Size:" << v.size() << endl;
+    cout << "Capacity:" << v.capacity() << endl;
+
     return 0;
 }
