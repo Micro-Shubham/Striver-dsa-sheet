8ad5cbb9de68fc95cefc480ac6620881b337df66
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-26 23:20:17 +0530

    M_PI

---
+++ vector/test.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    double r = 23;
+    double area = 0.0;
+    area = r * M_PI * r;
+    cout << area;
+    return 0;
+}
